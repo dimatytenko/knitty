41a525743c116643d6bb265d662fb4ad28bce3d8
Slava <110696118+sl7one@users.noreply.github.com>
2023-11-21 13:25:58 +0200

    create hero

--- src/containers/Main/index.tsx
+++ src/containers/Main/index.tsx
@@ -3,10 +3,13 @@ import { ProductsCategories } from '../ProductCategories';
 import { Globally } from '../Globally';
 import { Br } from '../../ui-kit/Br';
 import { About } from '../About';
+import { Hero } from '../Hero/Hero';
 
 export const Main = () => {
   return (
     <div>
+      <Hero />
+      <Br desktop={120} mobile={60} />
       <Benefits />
       <Br desktop={120} mobile={60} />
       <About />
