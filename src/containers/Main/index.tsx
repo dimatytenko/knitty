46d9ae31a853a47a1447874207386780c2cebc18
Marina825 <81462462+Marina825@users.noreply.github.com>
2023-11-23 15:53:15 +0200

    add Tags Row

--- src/containers/Main/index.tsx
+++ src/containers/Main/index.tsx
@@ -5,14 +5,16 @@ import { Br } from '../../ui-kit/Br';
 import { About } from '../About';
 import { Hero } from '../Hero/Hero';
 import { KnittsGallery } from '../KnittsGallery';
+
 export const Main = () => {
   return (
     <div>
       <Hero />
+
       <Br desktop={120} mobile={60} />
       <Benefits />
       <Br desktop={120} mobile={60} />
-< KnittsGallery />
+      <KnittsGallery />
       <Br desktop={120} mobile={60} />
       <About />
       <Br desktop={120} mobile={60} />
