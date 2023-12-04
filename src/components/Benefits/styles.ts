7515b09eaa1415f661dad43dfd551d6077b02f2d
Dima Tytenko <amgpetronass@gmail.com>
2023-12-04 11:36:16 +0200

    update benefits

--- src/components/Benefits/styles.ts
+++ src/components/Benefits/styles.ts
@@ -4,7 +4,6 @@ export const BenefitsWrapper = styled.div`
   padding: 36px 0;
   border-top: 1px solid ${({ theme }) => theme.palette.colors.line};
   border-bottom: 1px solid ${({ theme }) => theme.palette.colors.line};
-  cursor: pointer;
   &:hover ul {
     animation-play-state: paused;
   }
