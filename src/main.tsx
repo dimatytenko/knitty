4f30c21b0bf8f76d3e7586f3260d356d36edb43e
Slava <110696118+sl7one@users.noreply.github.com>
2023-12-26 19:02:35 +0200

    add fingerprint

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { FpjsProvider } from '@fingerprintjs/fingerprintjs-pro-react';
 
 import App from './App.tsx';
 
@@ -10,9 +10,16 @@ const vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    {/* <React.StrictMode> */}
-    <App />
-    {/* </React.StrictMode> */}
-  </BrowserRouter>,
+  <FpjsProvider
+    loadOptions={{
+      apiKey: 'ubJJOvwQyzytaEb6tAJx',
+    }}
+  >
+    <BrowserRouter>
+      {/* <React.StrictMode> */}
+      <App />
+      {/* </React.StrictMode> */}
+    </BrowserRouter>
+    ,
+  </FpjsProvider>,
 );
