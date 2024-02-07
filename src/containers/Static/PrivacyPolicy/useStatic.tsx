96be7fbf9122fb865e1132ffc8e4ee29dbe8a948
Slava <110696118+sl7one@users.noreply.github.com>
2024-02-07 19:39:25 +0200

    add translations PrivacyPolicy

---
+++ src/containers/Static/PrivacyPolicy/useStatic.tsx
@@ -0,0 +1,65 @@
+import { useTranslation } from 'react-i18next';
+
+export default function useStatic() {
+  const { t } = useTranslation();
+  const data = {
+    title: t('privacyPolicy.title'),
+    subtitle: t('privacyPolicy.subtitle'),
+    info: [
+    //   {
+    //     titleBlock: t('privacyPolicy.titleBlock.???'),
+    //     text: [t('privacyPolicy.text.???.0')],
+    //   },
+      {
+        titleBlock: t(
+          'privacyPolicy.titleBlock.PERSONAL INFORMATION WE COLLECT',
+        ),
+        text: [
+          t('privacyPolicy.text.PERSONAL INFORMATION WE COLLECT.0'),
+          t('privacyPolicy.text.PERSONAL INFORMATION WE COLLECT.1'),
+          t('privacyPolicy.text.PERSONAL INFORMATION WE COLLECT.2'),
+          t('privacyPolicy.text.PERSONAL INFORMATION WE COLLECT.3'),
+        ],
+      },
+      {
+        titleBlock: t(
+          'privacyPolicy.titleBlock.HOW DO WE USE YOUR PERSONAL INFORMATION?',
+        ),
+        text: [
+          t('privacyPolicy.text.HOW DO WE USE YOUR PERSONAL INFORMATION?.0'),
+          t('privacyPolicy.text.HOW DO WE USE YOUR PERSONAL INFORMATION?.1'),
+        ],
+      },
+      {
+        titleBlock: t(
+          'privacyPolicy.titleBlock.SHARING YOUR PERSONAL INFORMATION',
+        ),
+        text: [
+          t('privacyPolicy.text.SHARING YOUR PERSONAL INFORMATION.0'),
+          t('privacyPolicy.text.SHARING YOUR PERSONAL INFORMATION.1'),
+        ],
+      },
+      {
+        titleBlock: t('privacyPolicy.titleBlock.BEHAVIOURAL ADVERTISING'),
+        text: [
+          t('privacyPolicy.text.BEHAVIOURAL ADVERTISING.0'),
+          t('privacyPolicy.text.BEHAVIOURAL ADVERTISING.1'),
+          t('privacyPolicy.text.BEHAVIOURAL ADVERTISING.2'),
+        ],
+      },
+      {
+        titleBlock: t('privacyPolicy.titleBlock.DO NOT TRACK'),
+        text: [t('privacyPolicy.text.DO NOT TRACK.0')],
+      },
+      {
+        titleBlock: t('privacyPolicy.titleBlock.YOUR RIGHTS'),
+        text: [
+          t('privacyPolicy.text.YOUR RIGHTS.0'),
+          t('privacyPolicy.text.YOUR RIGHTS.1'),
+        ],
+      },
+    ],
+  };
+
+  return { data };
+}
